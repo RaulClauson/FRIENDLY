a601b699919b1df4af5382a864c05ab79f70740e
Raul Clauson <106999644+RaulClauson@users.noreply.github.com>
2024-08-24 22:17:37 -0300

    Add files via upload

---
+++ gsap.js
@@ -0,0 +1,27 @@
+gsap.to(".funcionalidades_pin", {
+    scrollTrigger:{
+        trigger: ".funcionalidades_pin",
+        start: "0% top",
+        end: "400% 0",
+        pin:true,
+        scrub: 1,
+    },
+    ease:"none",
+    opacity:1,
+    smooth: true,
+    duration: 0.3
+});
+
+gsap.to(".funcionalidades_pin_dentro", {
+    scrollTrigger:{
+        trigger: ".funcionalidades_pin_dentro",
+        start: "0% top",
+        end: "400% 0",
+        scrub: 1,
+    },
+    right:"0",
+    ease:"none",
+    opacity:1,
+    smooth: true,
+    duration: 0.3
+});
